b8ae1cd9ba93d888410e58981db72d46b295f935
Sergej Horvat <shorvat@mojemjesto.net>
2015-04-27 01:06:13 +0200

    Assigment 2 complete
    Upload ready!

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+
+makeCacheMatrix <- function(y = matrix()) {
+        
+        x <- y
+        i <- NULL
+        
+        ## Set matrix function
+        set <- function(y) {
+                x <<- y
+        }
+        
+        ## Get Matrix function
+        get <- function(){
+                x
+        }
+        ## Get inerse matrix
+        getInverse <- function(y){
+                i
+        }
+        ## Seet iverse matrix
+        setInverse <- function(y){
+                i <<- y
+        }
+        
+        #List functions
+        list(set = set , get = get, getInverse =getInverse, )
 
 }
 
@@ -12,4 +37,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        # is  inverse already calculated?
+        c <- x$getInverse()
+        if(!is.null(c)) {
+                message ("cached inverse matrix is:")
+                return(c)
+        }
+        nc <- x$get()
+        c <- solve(nc)
+        x$setInverse(c)
+        c
+        
 }
