c8a437203dbc004b3a7bf8363691741dcd9bc264
sergejhorvat <sergejhorvat@users.noreply.github.com>
2017-10-12 00:37:13 +0200

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,5 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 
 
@@ -22,7 +21,7 @@ makeCacheMatrix <- function(y = matrix()) {
         getInverse <- function(y){
                 i
         }
-        ## Seet iverse matrix
+        ## Set iverse matrix
         setInverse <- function(y){
                 i <<- y
         }
